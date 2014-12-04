5d846d0961e7bd74033d668cf528c6be61f2d390
Frank Groeneveld <frank@frankgroeneveld.nl>
2014-12-04 21:49:49 +0100

    Support incomplete sudoku parsing

--- src/parse.c
+++ src/parse.c
@@ -5,7 +5,10 @@ void load(int grid[])
 	int i;
 	char c;
 	
-	for (i = 0; i < 9 * 9; c = getchar())
+	for (i = 0; i < 9 * 9; c = getchar()) {
 		if (c >= '1' && c <= '9')
 			grid[i++] = c - '0';
+		if (c == ' ')
+			grid[i++] = 0;
+	}
 }
