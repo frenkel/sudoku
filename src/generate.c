2c0bbb7046d4e70e7bed562ba8fcfa2e02996e0b
Frank Groeneveld <frank@frankgroeneveld.nl>
2014-12-23 21:31:15 +0100

    Start generating (invalid) Sudoku

--- src/generate.c
+++ src/generate.c
@@ -1,8 +1,23 @@
+#include <stdlib.h>
+#include "verify.h"
+
 void generate(int *grid)
 {
 	int row;
 	int column;
-	for (row = 0; row < 9; row++)
+	int value;
+	int prev_value;
+
+	for (row = 0; row < 9; row++) {
 		for (column = 0; column < 9; column++)
 			grid[row * 9 + column] = 0;
+
+		for (value = 1; value <= 9; value++) {
+			column = arc4random() % 9;
+			prev_value = grid[row * 9 + column];
+			grid[row * 9 + column] = value;
+		}
+
+	}
+
 }
