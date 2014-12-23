97d47d9c6f5dd70216b99b2801c96b9ca7d8149b
Frank Groeneveld <frank@frankgroeneveld.nl>
2014-12-23 22:49:42 +0100

    Count 9's as well

--- src/verify.c
+++ src/verify.c
@@ -1,8 +1,8 @@
 int verify(int *grid, int complete)
 {
-	int found_h[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-	int found_v[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-	int found_b[9] = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	int found_h[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	int found_v[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+	int found_b[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 	int row;
 	int column;
 	int block;
@@ -11,9 +11,9 @@ int verify(int *grid, int complete)
 
 		for (column = 0; column < 9; column++) {
 			/* prevent overflow using modulo 9 */
-			found_h[grid[column + row * 9] % 9]++;
+			found_h[grid[column + row * 9] % 10]++;
 			/* switch column and row to walk vertical */
-			found_v[grid[row + column * 9] % 9]++;
+			found_v[grid[row + column * 9] % 10]++;
 
 			/* walk the block left to right, top to bottom
 			   by doing three steps, then skipping 9, etc */
@@ -21,10 +21,10 @@ int verify(int *grid, int complete)
 			/* walk all blocks left to right,
 			   skip to next block after every row */
 			block += row * 3 + (row / 3) * 18;
-			found_b[grid[block] % 9]++;
+			found_b[grid[block] % 10]++;
 		}
 
-		for (column = 0; column < 9; column++) {
+		for (column = 1; column <= 9; column++) {
 			if (found_h[column] > 1 || found_v[column] > 1
 					|| found_b[column] > 1)
 				return 0;
