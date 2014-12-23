483bcec330212ccbe8ff2275686ca6dde8b6a11b
Frank Groeneveld <frank@frankgroeneveld.nl>
2014-12-23 22:54:22 +0100

    Generate one pass of random valid numbers

--- src/generate.c
+++ src/generate.c
@@ -8,16 +8,18 @@ void generate(int *grid)
 	int value;
 	int prev_value;
 
-	for (row = 0; row < 9; row++) {
+	for (row = 0; row < 9; row++)
 		for (column = 0; column < 9; column++)
 			grid[row * 9 + column] = 0;
 
+	for (row = 0; row < 9; row++)
 		for (value = 1; value <= 9; value++) {
 			column = arc4random() % 9;
 			prev_value = grid[row * 9 + column];
 			grid[row * 9 + column] = value;
-		}
 
-	}
+			if (!verify(grid, 0))
+				grid[row * 9 + column] = prev_value;
+		}
 
 }
